4a7cd5d894ab7c44235c078c3838fe08623a0201
daridev <hernandezdarifrancisco@gmail.com>
2021-12-15 18:08:57 -0600

    Open matches: open each match in a new tab

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 import os
+import time
 from config import Config
 from logs import logger
+from tqdm import tqdm
 from scraping_manager.automate import Web_scraping
 
 def main (): 
@@ -27,8 +29,41 @@ def main ():
     scraper.click (selector_submit)
     scraper.refresh_selenium ()
 
-    input ("end?")
-  
+    # Pagination loop
+    while True:
+
+        # Get matches links
+        links_matches = []
+        selector_matches = '.next-matches-section.allchamp.active > .next-matches-section-page[data-nm-page="1"] > a.next-matches-item'
+        elems_matches = scraper.get_elems (selector_matches)
+        for match_index in range (1, len (elems_matches) + 1):
+
+            selector_link = f"{selector_matches}:nth-child({match_index})"
+            link_match = scraper.get_attrib (selector_link, "href")
+            links_matches.append (link_match)
+
+        # Open new empty tab
+        logger.info ("Extracting data: ")
+        scraper.open_tab ()
+        scraper.switch_to_tab (1)
+        for link in tqdm(links_matches): 
+
+            # Open match in second tab
+            scraper.set_page (link)
+            time.sleep (5)
+
+            # TODO:Get match data
+
+        # Close tab
+        scraper.end_browser ()
+        scraper.switch_to_tab (0) 
+
+        input ("Continue?")
+        selector_next = ".next-matches-btn.next"
+        classes_next = scraper.get_attrib (selector_next, "class")
+        if "disable" in classes_next:
+            logger.info ("No more matches. Program end.")
+
 
 
 if __name__ == "__main__":
